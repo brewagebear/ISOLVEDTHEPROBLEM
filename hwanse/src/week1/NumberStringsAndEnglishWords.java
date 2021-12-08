75c5e04e4736dc4c168b3eab95bfbef57477b6a2
hwanse <khsb2012@gmail.com>
2021-12-08 22:33:57 +0900

    프로그래머스 숫자 문자열과 영단어 문제 풀이

--- hwanse/src/week1/NumberStringsAndEnglishWords.java
+++ hwanse/src/week1/NumberStringsAndEnglishWords.java
@@ -1,2 +1,43 @@
-package week1;public class NumberStringsAndEnglishWords {
+package week1;
+
+import java.util.regex.Pattern;
+
+public class NumberStringsAndEnglishWords {
+
+    public int solution(String s) {
+        String tempString = s;
+
+        while (!isNumbers(tempString)) {
+            for (Number type : Number.values()) {
+                String numberString = type.name().toLowerCase();
+
+                if (tempString.contains(numberString)) {
+                    tempString = tempString.replaceAll(numberString, String.valueOf(type.getValue()));
+                    break;
+                }
+            }
+        }
+
+        return Integer.parseInt(tempString);
+    }
+
+    public boolean isNumbers(String numberString) {
+        return Pattern.matches("^[0-9]+$", numberString);
+    }
+
+}
+
+enum Number {
+    ZERO(0), ONE(1), TWO(2), THREE(3), FOUR(4),
+    FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9);
+
+    private final int value;
+
+    Number(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
